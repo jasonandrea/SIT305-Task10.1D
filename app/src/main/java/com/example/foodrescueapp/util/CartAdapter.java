e816665cba3a2d96cc7de530508c2365b9769903
jasonandrea14 <45784146+jasonandrea@users.noreply.github.com>
2021-06-04 12:19:49 +1000

    Changes: CartAdapter.java - Added new java class for RecyclerView adapter in CartActivity

---
+++ app/src/main/java/com/example/foodrescueapp/util/CartAdapter.java
@@ -0,0 +1,54 @@
+package com.example.foodrescueapp.util;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.foodrescueapp.R;
+import com.example.foodrescueapp.model.Food;
+
+import java.util.List;
+
+public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder> {
+    // Instance variables
+    private List<Food> foods;
+    private Context context;
+
+    // Constructor
+    public CartAdapter(List<Food> foods, Context context) {
+        this.foods = foods;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CartAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(this.context).inflate(R.layout.cart, parent, false);
+        return new CartAdapter.ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CartAdapter.ViewHolder holder, int position) {
+        holder.foodTitle.setText(this.foods.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.foods.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // Instance variable
+        private TextView foodTitle;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            foodTitle = itemView.findViewById(R.id.cartFoodNameTextView);
+        }
+    }
+}
